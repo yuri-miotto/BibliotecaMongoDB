3debd7a5a210a5633fb6bddacaa53c469ae7485c
yuri-miotto <yurimagrinimiotto@gmail.com>
2023-04-14 08:37:20 -0300

    Add project files.

---
+++ BibliotecaMongoDB/Livro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BibliotecaMongoDB
+{
+    [BsonIgnoreExtraElements]
+    internal class Livro
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("isbn")]
+        [BsonRepresentation(BsonType.Int32)]
+        public int ISBN { get; set; }
+
+        [BsonElement("titulo")]
+        public string? Titulo { get; set; }
+
+        [BsonElement("editora")]
+        public string? Editora { get; set; }
+
+        [BsonElement("ano publicacao")]
+        [BsonRepresentation(BsonType.Int32)]
+        public int AnoPublicacao { get; set; }
+
+        public Livro(int isbn, string? titulo, string? editora, int ano_publicacao)
+        {
+            ISBN = isbn;
+            Titulo = titulo;
+            Editora = editora;
+            AnoPublicacao = ano_publicacao;
+        }
+
+
+        public override string ToString()
+        {
+            return $"ISBN: {ISBN}\nTítulo: {Titulo}\nEditora: {Editora}\nAno de publicação: {AnoPublicacao}\n";
+        }
+    }
+}
---
+++ BibliotecaMongoDB/Program.cs
@@ -0,0 +1,78 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using BibliotecaMongoDB;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MongoClient mongo = new MongoClient("mongodb://localhost:27017");
+
+        var basededados = mongo.GetDatabase("biblioteca");
+        var collection = basededados.GetCollection<BsonDocument>("livro");
+        var documents = collection.Find(new BsonDocument()).ToList();
+
+        //encontrar um livro na biblioteca:
+        //Console.WriteLine("Informe o título do livro: ");
+        //string t = Console.ReadLine();
+        //var filtro = Builders<BsonDocument>.Filter.Regex("titulo", t);
+        //var l = collection.Find(filtro).FirstOrDefault();
+        //var livro = BsonSerializer.Deserialize<Livro>(l);
+        //Console.WriteLine("\n" + livro.ToString());
+
+
+        //alterar a editora do livro:
+        //Console.WriteLine("Informe o título do livro: ");
+        //string t = Console.ReadLine();
+        //var l = collection.Find(Builders<BsonDocument>.Filter.Regex("titulo", t)).First();
+        //var livro = BsonSerializer.Deserialize<Livro>(l);
+        //Console.WriteLine("Informe a nova editora: ");
+        //string e = Console.ReadLine();
+        //livro.Editora = e;
+        //var filtro = Builders<BsonDocument>.Filter.Regex("titulo", t);
+        //var update = Builders<BsonDocument>.Update.Set("editora", e);
+        //collection.UpdateOne(filtro, update);
+
+
+        //excluir um livro da biblioteca:
+        //Console.WriteLine("Informe o título do livro: ");
+        //string t = Console.ReadLine();
+        //var filtro = Builders<BsonDocument>.Filter.Regex("titulo", t);
+        //collection.FindOneAndDelete(filtro);
+
+
+        //listar os livros da biblioteca:
+        //foreach (var livro in documents)
+        //{
+        //    Console.WriteLine(livro.ToString());
+        //    Console.ReadLine();
+        //}
+
+
+        //inserir um livro na biblioteca:
+        Console.WriteLine("ISBN: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Título: ");
+        string t = Console.ReadLine();
+
+        Console.WriteLine("Editora: ");
+        string e = Console.ReadLine();
+
+        Console.WriteLine("Ano de publicação: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Livro livro = new(n, t, e, a);
+        Console.WriteLine(livro.ToString());
+        var l = new BsonDocument
+        {
+            {"isbn", livro.ISBN},
+            {"titulo", livro.Titulo},
+            {"editora", livro.Editora},
+            {"ano publicacao", livro.AnoPublicacao},
+        };
+        Console.WriteLine(l);
+        collection.InsertOne(l);
+    }
+}
